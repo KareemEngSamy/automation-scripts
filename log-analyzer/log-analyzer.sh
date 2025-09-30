1f652539540a10f8661321c8b6ce56d27a585f2d
Kareem Samy <karimkrim2014@gmail.com>
2025-09-30 20:17:04 +0300

    fixed syntax errors

--- log-analyzer/log-analyzer.sh
+++ log-analyzer/log-analyzer.sh
@@ -8,24 +8,25 @@ echo "Analysing log files:" > "$REPORT_FILE"
 echo "====================" >> "$REPORT_FILE"
 
 echo -e "\nList of log files updated in last 24 hours:" >> "$REPORT_FILE"
-LOG_FILES=$(find  $LOG_DIR -name "*.log" -mtume -1)
+LOG_FILES=$(find "$LOG_DIR" -name "*.log" -mtime -1)
 echo "$LOG_FILES" >> "$REPORT_FILE"
 
 for LOG_FILE in $LOG_FILES; do
     echo -e "\n" >> "$REPORT_FILE"
     echo "===============================================" >> "$REPORT_FILE"
-    echo "==================$Log_File====================" >> "$REPORT_FILE"
+    echo "==================$LOG_FILE====================" >> "$REPORT_FILE"
     echo "===============================================" >> "$REPORT_FILE"
 
     for PATTERN in ${ERROR_PATTERNS[@]}; do
         echo -e "\nsearching for $PATTERN logs in $LOG_FILE file:" >> "$REPORT_FILE"
         grep "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
 
-        echo -e "\nNumber of $PATTERN logs in $LOG_FILE file:" >> $REPORT_FILE
+        echo -e "\nNumber of $PATTERN logs in $LOG_FILE file:" >> "$REPORT_FILE"
 
         ERROR_COUNT=$(grep -c "$PATTERN" "$LOG_FILE")
-        echo "$ERROR_COUNT" >> $REPORT_FILE
-        if[ "$ERROR_COUNT" -gt 10 ]; then
+        echo "$ERROR_COUNT" >> "$REPORT_FILE"
+
+        if [ "$ERROR_COUNT" -gt 10 ]; then
             echo -e "\n!!!! Action Required !!!! Too many $PATTERN issues in log file $LOG_FILE"
         fi
     done
